ea7557e21a7e434d0a6d06799a61b3ddd89ae16d
Nadav Rutman Moshe <nadav8333@gmail.com>
2024-11-18 20:10:25 +0200

    double-dockerfile

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,13 +1,10 @@
 #include <gtest/gtest.h>
-
-int add(int a, int b) {
-    return a + b;
-}
+#include "math.h"
 
 TEST(AdditionTest, PositiceNumbers){
     EXPECT_EQ(add(1,2),3);
 }
-int main(int argc, char ** rgv){
+int main(int argc, char ** argv){
     ::testing::InitGoogleTest(&argc,argv);
     return RUN_ALL_TESTS();
 }
